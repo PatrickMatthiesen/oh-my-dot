b8cbea43e21841ba76daaa2d1d605cc3b747a037
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2024-03-04 22:49:40 +0100

    start on git init

--- cmd/init.go
+++ cmd/init.go
@@ -13,9 +13,9 @@ func init() {
 	initcmd.MarkFlagsOneRequired("url")
 	viper.BindPFlag("remote-url", initcmd.Flags().Lookup("url"))
 
-	initcmd.Flags().StringP("repo", "r", "", "Path to the root of the dotfiles repository")
-	initcmd.MarkFlagDirname("repo")
-	viper.BindPFlag("repo-path", initcmd.Flags().Lookup("repo"))
+	initcmd.Flags().StringP("folder", "f", "", "Path to the root of the dotfiles repository")
+	initcmd.MarkFlagDirname("folder")
+	viper.BindPFlag("repo-path", initcmd.Flags().Lookup("folder"))
 
 	// initcmd.Flags().BoolP("force", "f", false, "Force initialization even if the directory is not empty")
 	rootCmd.AddCommand(initcmd)
@@ -28,11 +28,10 @@ var initcmd = &cobra.Command{
 Makes a git repository and sets remote origin to the specified URL.
 Default URL is $HOME/dotfiles but can be changed with the --url flag.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		rootGitRepoPath := viper.GetString("repo-path")
-		fmt.Println("rootGitRepoPath:", rootGitRepoPath)
+		util.InitGitRepo(viper.GetString("repo-path"), viper.GetString("remote-url"))
+		fmt.Println("Initialized dotfiles repo")
 
-		remoteUrl := viper.GetString("remote-url")
-		fmt.Println("remoteUrl:", remoteUrl)
+
 	},
 	PreRun: func(cmd *cobra.Command, args []string) {
 		// util.EnsureConfigFolder()
--- util/repo.go
+++ util/repo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/config"
 )
 
 func MoveAndAddFile() {
@@ -20,4 +21,15 @@ func getRepo(rootGitRepoPath string) {
 	Worktree, _ := r.Worktree()
 
 	fmt.Println("Worktree", Worktree)
+}
+
+func InitGitRepo(rootGitRepoPath string, remoteUrl string) {
+	r, err := git.PlainInit(rootGitRepoPath, false)
+	if err != nil {
+		fmt.Println("Error initializing git repo")
+	}
+	r.CreateRemote(&config.RemoteConfig{
+		Name: "origin",
+		URLs: []string{remoteUrl},
+	})
 }
