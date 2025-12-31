e27ae2247a4dc8bb3341b708d2ceaddbba7a72ac
Patrick Matthiesen <43612965+PatrickMatthiesen@users.noreply.github.com>
2025-12-31 13:55:01 +0100

    Delete version.go

--- version.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// Version is the current version of oh-my-dot
-// This version should follow semantic versioning (e.g., "1.0.0")
-// and will be used for self-update checks and version reporting
-const Version = "0.0.20"
